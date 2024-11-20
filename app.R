abf054d978ecea7bc8aa82d662f9d782e95bc264
lusig84 <lusig84@gmail.com>
2024-11-20 14:38:40 -0500

    11/20 2:38pm

--- app.R
+++ app.R
@@ -1,5 +1,20 @@
-#test
-library(readr)  # For read_csv function
+#cleaning data
+raw_data <- suppressWarnings(readLines("AtlanticCounty_Age.csv")) # Read the CSV file line by line, ignoring any warnings (e.g., incomplete last line).
+data_lines <- raw_data[-c(1:2)] # Remove the first two lines of extra info from the data.
+header <- unlist(strsplit(data_lines[1], ","))  # Extract the first line as header
+header <- gsub(" ", "_", header) # Replace spaces in the header names with underscores for consistency.
+AtlanticCounty_Age <- read.csv(text = paste(data_lines[-1], collapse = "\n"), header = FALSE) # Read the remaining lines as a CSV, treating the header separately.
+View(AtlanticCounty_Age)
+colnames(AtlanticCounty_Age) <- header # Assign the cleaned header names to the data frame's columns.
+AtlanticCounty_Age[AtlanticCounty_Age == "No Data"] <- NA # Replace "No Data" entries in the data frame with `NA` (missing data).
+AtlanticCounty_Age <- AtlanticCounty_Age[-19, ] # Remove row 19 from the data frame, which contains unwanted data.
+AtlanticCounty_Age <- AtlanticCounty_Age[, -ncol(AtlanticCounty_Age)] # Remove the last column of the data frame cause its problematic.
+AtlanticCounty_Age$Year <- as.integer(AtlanticCounty_Age$Year) # Convert the `Year` column to integers, replacing invalid entries with `NA`.
+cols_to_numeric <- setdiff(names(AtlanticCounty_Age), "Year")  # Identify all column names except "Year" for numeric conversion.
+AtlanticCounty_Age[cols_to_numeric] <- lapply(AtlanticCounty_Age[cols_to_numeric], as.numeric) # Convert all selected columns to numeric, ensuring they can be used for calculations.
+
+library(readr)  
+# For read_csv function
 
 # Get a list of all CSV files in the current directory
 csv_files <- list.files(pattern = "\\.csv$")
